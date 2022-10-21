188a5bdcd93e02dcc03a67e145c58f928fe8beee
WilliamSmithEdward <116382973+WilliamSmithEdward@users.noreply.github.com>
2022-10-21 11:18:54 -0700

    Add files via upload

---
+++ Tablelamp3.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+########################################################################
+# Filename    : Tablelamp3.py
+# Description : DIY MINI table lamp
+# Author      : www.freenove.com
+# modification: 2019/12/27
+########################################################################
+from gpiozero import LED, Button
+from signal import pause
+from time import sleep
+
+print ('Program is starting ... ')
+
+led = LED(17) # define LED pin according to BCM Numbering
+button = Button(18) # define Button pin according to BCM Numbering
+
+strobe = 0
+
+def onButtonPressed():
+    global strobe
+    strobe += 1
+    if strobe == 6:
+        strobe = 0
+    print(strobe)
+
+button.when_pressed = onButtonPressed
+
+while True:
+    if strobe == 0:
+        led.off()
+    if strobe == 1:
+        led.on()
+    if strobe == 2:
+        while strobe == 2:
+            led.toggle()
+            sleep(1)
+    if strobe == 3:
+        while strobe == 3:
+            led.toggle()
+            sleep(.5)
+    if strobe == 4:
+        while strobe == 4:
+            led.toggle()
+            sleep(.05)
+    if strobe == 5:
+        led.off()
+        strobeLength = 0
+        while strobe == 5:
+            led.on()
+            if strobeLength == 0:
+                sleep(1)
+                strobeLength = 1
+            else:
+                sleep(.25)
+                strobeLength = 0
+            led.off()
+            sleep(1)
